1240681432d6c34e218d5349661ae8b68277a823
NvDuc-JV260327 <Nvduc9620@gmail.com>
2026-05-19 19:52:46 +0700

    ex3.js

--- ex3.js
+++ ex3.js
@@ -1,9 +1,14 @@
 function sum(a, b) {
     if(isNaN(Number(a)) || isNaN(Number(b))) {
-        return "Nhập vào số";
+        return "Nhập vào số"
     }
     return a + b;
 }
 let a = +prompt("Nhập số thứ nhất");
 let b = +prompt("Nhập số thứ 2");
-alert("Tổng của 2 số là : " +sum(a, b));
+let result = sum(a, b);
+if(typeof result === "string") {
+    alert(result);
+} else {
+    alert("Tổng của 2 số là: " +result);
+}
